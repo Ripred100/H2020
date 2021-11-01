f7456f40f8e5ec038e833fd5af813790c0f534f2
Ripred100 <60947523+Ripred100@users.noreply.github.com>
2021-11-01 16:17:33 -0400

    sorta works

--- anu.py
+++ anu.py
@@ -3,15 +3,18 @@
 #S = Level you must go to
 
 # case 1:
-TCases = input()
+TCases = int(input())
 
 
 for i in range(TCases):
-    N,K,S = input().split()
+    N,K,S = input().strip().split()
+    N = int(N)
+    K = int(K)
+    S = int(S)
     TimeGoBack = (K-1) + (K-S) + (N-S + 1)
     TimeRestart = (K-1) + N
     minTime = min(TimeGoBack,TimeRestart)
-    print(f'Case #{i}: {minTime}')
+    print(f'Case #{i+1}: {minTime}')
 
 
 
