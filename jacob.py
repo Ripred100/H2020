f1a943611ab86b67e6580e6b12bc2d43731a63ae
jmutton2 <jmutton2@uwo.ca>
2021-11-01 16:19:16 -0400

    push

--- jacob.py
+++ jacob.py
@@ -1,1 +1,12 @@
-print("hello, neighboor")
+n = 8 #Number of levels
+k = 4 #Current Pos
+s = 2 #Sword pos
+testCases = int(input())
+for j in range(testCases):
+    n, k, s = map(int,input().strip().split())
+
+    goBack = (k-1) + (k-s) + (n-s) + 1
+    restart = (k-1) + n
+    m1 = max(goBack, restart)
+    print(f'Case #{i}: {min1}')
+
