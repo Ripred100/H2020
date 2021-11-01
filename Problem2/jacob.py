0f3bc90473cd21948583fda3b11d0dc6e441475a
jmutton2 <jmutton2@uwo.ca>
2021-11-01 17:25:08 -0400

    lel

--- Problem2/jacob.py
+++ Problem2/jacob.py
@@ -1,14 +1,20 @@
+def func1(x):
+    for n in range(len(x)):
+        index = n
+        value = int(x[n])
 
-def func1(value, index):
-    int(value)
-    if (index%2 == 0):
-        
+        if (index%2 != value%2):
+            return False
+
+    return True
 
 testCases = int(input())
 for j in range(testCases):
     l, r = map(int, input().strip().split())
+    count = 0
+    for t in range(l,r+1):
+        x = str(t)
+        if func1(x):
+            ++count
+    print(f'Case #{j+1}: {count}')
 
-t = str(12145)
-
-for n in str(12145):
-    map(func1, (t[n], n))
