a2bc8b5294307fdbc6f8034a31e4e6ec999321f9
jmutton2 <jmutton2@uwo.ca>
2021-11-01 17:03:11 -0400

    leep[

---
+++ Problem2/jacob.py
@@ -0,0 +1,14 @@
+
+def func1(value, index):
+    int(value)
+    if (index%2 == 0):
+        
+
+testCases = int(input())
+for j in range(testCases):
+    l, r = map(int, input().strip().split())
+
+t = str(12145)
+
+for n in str(12145):
+    map(func1, (t[n], n))
